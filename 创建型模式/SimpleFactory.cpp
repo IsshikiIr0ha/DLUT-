bcca5a4c5d499968231857ee4992bc2071003447
IsshikiIr0ha <75306340+IsshikiIr0ha@users.noreply.github.com>
2021-10-20 19:14:45 +0800

    Create SimpleFactory.cpp

---
+++ 创建型模式/SimpleFactory.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+/**
+ * 简单工厂模式，只需要某种类的一个参数，就能返回一个相应的类指针。
+ * 将产品抽象，由具体子类实现
+ */
+class Operation{
+    private:
+        double a = 3, b = 3;
+    public:
+        virtual double getResult() = 0;
+        double getA() { return a; }
+        double getB() { return b; }
+        void setA(double a) { 
+            this->a = a; 
+        }
+        void setB(double b) { 
+            this->b = b; 
+        }
+};
+
+class Add:public Operation{
+    double getResult(){
+        return this->getA() + this->getB();
+    }
+};
+
+class Minus:public Operation{
+    double getResult(){
+        return this->getA() - this->getB();
+    }
+};
+
+class Multiple:public Operation{
+    double getResult(){
+        return this->getA() * this->getB();
+    }
+};
+
+class Divide:public Operation{
+    double getResult(){
+        if(this->getB())
+            return this->getA() / this->getB();
+        cout << "B can't be 0" << endl;
+        return 0;
+    }
+};
+
+class Factory{
+    public:
+    static Operation* getOperation(char c){
+        switch (c)
+            {
+            case '+':
+                return new Add;
+                break;
+            case '-':
+                return new Minus;
+                break;
+            case '*':
+                return new Multiple;
+                break;
+            case '/':
+                return new Divide;
+                break;
+            default:
+                return nullptr;
+            }
+    }
+};
+
+int main(){
+    Operation *op = Factory::getOperation('/');
+    printf("%f", op->getResult());
+}
